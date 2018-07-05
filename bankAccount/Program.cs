5b417f5dc1eb76778c675b154389db07c5d15dec
Kyle Mayers <kyle7150@gmail.com>
2018-07-05 19:03:26 +0000

    my finished version of jamals program

--- bankAccount/Program.cs
+++ bankAccount/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,57 +16,70 @@ namespace bankAccount
     public abstract class BankAccount
     {
         //need 4 attributes
-        int total { get; set; }
+        public int total { get; set; }
         int id { get; set; }
         int customerName { get; set; }
         TransactionQueue accountTransactions { get; set; }
 
-        public void ApplyTransaction()
+        public void ApplyTransaction(int trans)
         {
-
+            total = +trans;
         }
     }
-    public class CheckingAccount: BankAccount
+    public class CheckingAccount : BankAccount
     {
         int ServiceFee { get; set; }
 
         //Create transaction for yearly service fee
-        public Transaction newServiceFee()
+        public void newServiceFee()
         {
-            return new Transaction();
+            var yearlyFee = 100;
+            ApplyTransaction(yearlyFee);
         }
-         //submit the transaction to the queue
+        //submit the transaction to the queue
 
     }
     public class SavingAccount : BankAccount
     {
         int interestRate { get; set; }
         Transaction interest { get; set; }
+
+        public void interestCharge()
+        {
+            var interest = total + interestRate;
+            ApplyTransaction(interest);
+        }
     }
-    public class Transaction
+    public class Transaction : BankAccount
     {
         int amount { get; set; }
         DateTime date { get; set; }
         string type { get; set; }
-        
+
         //Transactions only come as two base types (you must deduce the types). 
         //Accounts can have more complicated transactions however when in a bank account a transaction only affects one item deduce that item.
     }
-    public class TransactionQueue
+    public class TransactionQueue : BankAccount
     {
         List<Transaction> transactions;
         void enque(Transaction trans)
         {
-
+            transactions.Add(trans);
         }
 
         void deque()
         {
-
+            var transactionAmount = Convert.ToInt32(transactions[0]);
+            ApplyTransaction(transactionAmount);
+            transactions.RemoveAt(0);
         }
-        void enque (string r)
+        public void request(string r)
         {
-            return
+            deque();
+        }
+        public void request(Transaction tranny)
+        {
+            enque(tranny);
         }
     }
 }
